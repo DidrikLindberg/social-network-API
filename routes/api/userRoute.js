ec45add82425d0747b8d03bab9e60b14367cc43d
GitDidrik <lindberg.didrik@gmail.com>
2023-05-22 14:57:18 -0700

    routes update

---
+++ routes/api/userRoute.js
@@ -0,0 +1,21 @@
+
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userController');
+
+
+router.route('/').get(getUsers).post(createUser);
+
+
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+
+router.route('/:userId/friends/:friendsId').delete(removeFriend).post(addFriend);
+
+module.exports = router;
